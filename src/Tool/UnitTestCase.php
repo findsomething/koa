e5bbddf64f08f23054e0151f601cf25e47c8b775
qiqisan <qiqisan1@126.com>
2017-10-11 18:02:10 +0800

    fix unitTestCase

--- src/Tool/UnitTestCase.php
+++ src/Tool/UnitTestCase.php
@@ -2,6 +2,24 @@
 
 namespace FSth\Koa\Tool;
 
+use FSth\Co\Call;
+
 class UnitTestCase extends \PHPUnit_Framework_TestCase
 {
+    protected function runGo(callable $func)
+    {
+        Call::go(function () use ($func) {
+            yield $this->clear();
+            yield $func();
+        }, function ($result, $e) {
+            if ($e) {
+                throw $e;
+            }
+        });
+    }
+
+    protected function clear()
+    {
+
+    }
 }
